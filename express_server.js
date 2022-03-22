b1c044b467d92910ecb371841b6dc7734eb5636b
prashanthk02 <prashanth.k369@gmail.com>
2022-03-22 19:29:01 -0400

    cookie parser setup and create a login form on client side

--- express_server.js
+++ express_server.js
@@ -5,7 +5,8 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); //set ejs view engine
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require("cookie-parser");
+app.use(bodyParser.urlencoded({extended: true}), cookieParser());
 
 const generateRandomString = function() {
   return Math.random().toString(36).substring(2, 8);
