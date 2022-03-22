7a1b68b396f6dce48a6b2f8096e96d0668d07d54
prashanthk02 <prashanth.k369@gmail.com>
2022-03-21 22:30:26 -0400

    added a feature to redirect shortURL link to longURL

--- express_server.js
+++ express_server.js
@@ -44,6 +44,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body);
   const shortURL = generateRandomString();
