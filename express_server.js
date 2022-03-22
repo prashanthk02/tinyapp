bc17cdda12af8611122572d36052486ebca15757
prashanthk02 <prashanth.k369@gmail.com>
2022-03-21 20:49:46 -0400

    add a function to generate random string of 6 alphanumeric charcters

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs"); //set ejs view engine
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const generateRandomString = function () {
+  return Math.random().toString(36).substring(2, 8);
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
